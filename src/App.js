45cc5e0e9cf596a60a8d9d346a288e3daf1e0ccc
manish7479dlp <manish7479dlp@gmail.com>
2023-11-21 09:35:02 +0530

    ui: create sidebar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { ColorModeContext , useMode } from './theme'
 import { CssBaseline , ThemeProvider, Typography } from '@mui/material'
 import Topbar from "../src/pages/global/Topbar"
+import Sidebar from './pages/global/Sidebar'
+// import Test from './components/Test'
 
 const App = () => {
   const [theme , colorMode] = useMode()
@@ -10,6 +12,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <CssBaseline/>
           <div className='app'>
+            <Sidebar/>
             <div className='content'>
               <Topbar/>
             </div>
