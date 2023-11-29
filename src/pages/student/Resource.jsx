9236369a7f3e783fc864919ce71376977d0e98a7
manish7479dlp <manish7479dlp@gmail.com>
2023-11-29 09:41:35 +0530

    ui: create resource page

---
+++ src/pages/student/Resource.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import Header from '../../components/Header'
+
+const Resource = () => {
+  return (
+      <Box m="20px">
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Header title="RESOURCE" subtitle="Welcome to Resource Section" />
+
+          </Box>
+      </Box>
+  )
+}
+
+export default Resource
