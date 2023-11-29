3e759ab2fa137f88a7af3d334f03de5102c8208f
manish7479dlp <manish7479dlp@gmail.com>
2023-11-29 09:39:42 +0530

    ui: create finance page

---
+++ src/pages/student/Finance.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import Header from '../../components/Header'
+
+const Finance = () => {
+  return (
+      <Box m="20px">
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Header title="FINANCE" subtitle="Welcome to Finance Section" />
+
+          </Box>
+      </Box>
+  )
+}
+
+export default Finance
