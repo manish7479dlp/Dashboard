9fb4500913ff7fedfa5c8d53b51c3bd789c4e616
manish7479dlp <manish7479dlp@gmail.com>
2023-11-30 18:50:57 +0530

    ui:create dashboard

--- src/pages/global/Sidebar.jsx
+++ src/pages/global/Sidebar.jsx
@@ -115,7 +115,7 @@ const Sidebar = () => {
           <Box paddingLeft={isCollapsed ? undefined : "10%"}>
             <Item
               title="Dashboard"
-              to="/dashboard"
+              to="/"
               icon={<HomeOutlinedIcon />}
               selected={selected}
               setSelected={setSelected}
