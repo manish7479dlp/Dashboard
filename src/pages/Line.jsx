aa67f77f22992652521f279d1a1ab71cd07e9ef4
manish7479dlp <manish7479dlp@gmail.com>
2023-11-30 19:37:27 +0530

    reset: height

--- src/pages/Line.jsx
+++ src/pages/Line.jsx
@@ -6,7 +6,7 @@ const Line = () => {
   return (
     <Box m="20px">
       <Header title="Line Chart" subtitle="Simple Line Chart" />
-      <Box height="75vh">
+      <Box height="70vh">
         <LineChart />
       </Box>
     </Box>
