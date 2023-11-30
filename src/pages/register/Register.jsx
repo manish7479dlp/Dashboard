def4ee87e884b94c341ffda8072f9a3d2f7c4c04
manish7479dlp <manish7479dlp@gmail.com>
2023-11-30 11:48:14 +0530

    ui:create register page

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,264 @@
+import { Box, Button, TextField } from "@mui/material";
+import { Formik, Field } from "formik";
+import * as yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../../components/Header";
+
+const Register = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Box m="20px">
+      <Header
+        title="New Registration"
+        subtitle="Create a New Student Profile"
+      />
+
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={checkoutSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+              sx={{
+                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+                // "& > MuiFormLabel-root-MuiInputLabel-root.Mui-focused" : {color: "white"}
+              }}
+            >
+              {/* <Field
+                name="color"
+                component="select"
+                value={values.color}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              >
+                <option value="">***Select***</option>
+                <option value="red">Red</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+              </Field> */}
+
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="First Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.firstName}
+                name="firstName"
+                error={!!touched.firstName && !!errors.firstName}
+                helperText={touched.firstName && errors.firstName}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Last Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.lastName}
+                name="lastName"
+                error={!!touched.lastName && !!errors.lastName}
+                helperText={touched.lastName && errors.lastName}
+                sx={{ gridColumn: "span 2" }}
+              />
+
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Father's Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.fathersName}
+                name="fathersName"
+                error={!!touched.fathersName && !!errors.fathersName}
+                helperText={touched.fathersName && errors.fathersName}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="number"
+                label="Aadhar Number"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.aadharNumber}
+                name="aadharNumber"
+                error={!!touched.aadharNumber && !!errors.aadharNumber}
+                helperText={touched.aadharNumber && errors.aadharNumber}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Department"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.department}
+                name="department"
+                error={!!touched.department && !!errors.department}
+                helperText={touched.department && errors.department}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                error={!!touched.email && !!errors.email}
+                helperText={touched.email && errors.email}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="Address"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address}
+                name="address"
+                error={!!touched.address && !!errors.address}
+                helperText={touched.address && errors.address}
+                sx={{ gridColumn: "span 3" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="number"
+                label="Contact Number"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.contact}
+                name="contact"
+                error={!!touched.contact && !!errors.contact}
+                helperText={touched.contact && errors.contact}
+                sx={{ gridColumn: "span 1" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="City"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.city}
+                name="city"
+                error={!!touched.city && !!errors.city}
+                helperText={touched.city && errors.city}
+                sx={{ gridColumn: "span 1" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="District"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.district}
+                name="district"
+                error={!!touched.district && !!errors.district}
+                helperText={touched.district && errors.district}
+                sx={{ gridColumn: "span 1" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="number"
+                label="Pincode"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.pincode}
+                name="pincode"
+                error={!!touched.pincode && !!errors.pincode}
+                helperText={touched.pincode && errors.pincode}
+                sx={{ gridColumn: "span 1" }}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                type="text"
+                label="State"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.state}
+                name="state"
+                error={!!touched.state && !!errors.state}
+                helperText={touched.state && errors.state}
+                sx={{ gridColumn: "span 1" }}
+              />
+            </Box>
+            <Box display="flex" justifyContent="center" mt="20px">
+              <Button type="submit" color="secondary" variant="contained">
+                Register
+              </Button>
+            </Box>
+          </form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const checkoutSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  fathersName: yup.string().required("required"),
+  department: yup.string().required("required"),
+  aadharNumber: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  contact: yup
+    .string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("required"),
+  address: yup.string().required("required"),
+  district: yup.string().required("required"),
+  city: yup.string().required("required"),
+  pincode: yup.string().required("required"),
+  state: yup.string().required("required"),
+  color: yup.string().required("required"),
+});
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  fathersName: "",
+  aadharNumber: "",
+  department: "",
+  email: "",
+  contact: "",
+  address: "",
+  district: "",
+  city: "",
+  pincode: "",
+  state: "",
+  color: "",
+};
+
+export default Register;
