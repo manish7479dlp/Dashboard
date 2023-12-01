afec0dea57b1e8a9078b6f1f38fd340589280062
manish7479dlp <manish7479dlp@gmail.com>
2023-12-01 20:18:06 +0530

    ui:create and implement full academicSight page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Dashboard from './pages/dashboard/Dashboard'
 import { Route, Routes } from 'react-router-dom'
 import Team from './pages/Team'
 import Contact from "./pages/Contact"
-import Academic from "./pages/student/Academic"
-import Finance from './pages/student/Finance'
 import Resource from './pages/student/Resource'
 import Register from './pages/register/Register'
 import Bar from './pages/Bar'
@@ -16,7 +14,8 @@ import Pie from './pages/Pie'
 import Line from './pages/Line'
 import StudentList from './pages/student/StudentList'
 import StudentRegister from './pages/student/StudentRegister'
-// import Test from './components/Test'
+import AcademicInsight from "./pages/student/AcademicInsights"
+
 
 const App = () => {
   const [theme , colorMode] = useMode()
@@ -32,12 +31,11 @@ const App = () => {
                 <Route path="/" element={<Dashboard/>}/>
                 <Route path="/student-list" element={<StudentList/>}/>
                 <Route path="/student-register" element={<StudentRegister/>}/>
+                <Route path="/academic-insights" element={<AcademicInsight/>}/>
 
 
                 <Route path="/team" element={<Team/>}/>
                 <Route path="/contact" element={<Contact/>}/>
-                <Route path="/academic" element={<Academic/>}/>
-                <Route path="/finance" element={<Finance/>}/>
                 <Route path="/resource" element={<Resource/>}/>
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/barchart" element={<Bar/>}/>
---
+++ src/pages/student/AcademicInsights.jsx
@@ -0,0 +1,236 @@
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { mockTransactions } from "../../assets/TempData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import AddIcon from '@mui/icons-material/Add';
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import Header from "../../components/Header";
+import StatBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import BarChart from "../../components/BarChart";
+import PieChart from "../../components/PieChart";
+import LineChart from "../../components/LineChart";
+
+// import LineChart from "../../components/LineChart";
+// // import BarChart from "../../components/BarChart";
+// import StatBox from "../../components/StatBox";
+// import ProgressCircle from "../../components/ProgressCircle";
+// import PieChart from "../../components/PieChart";
+
+const AcademicInsights = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const downloadReport = () => {
+    alert("It's is not implemented yet..")
+  }
+
+  return (
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="ACADEMIC INSIGHTS" subtitle="Welcome to Academic Details Section" />
+
+       {/* button */}
+        {/* <Box>
+          <Button
+            onClick={downloadReport}
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box> */}
+      </Box>
+
+      {/* GRID & CHARTS */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+        {/* ROW 1 */}
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="150"
+            subtitle="New Admission"
+            progress="0.47"
+            increase="+5%"
+            icon={
+              <PersonAddIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="63"
+            subtitle="Enrolled Student"
+            progress="0.63"
+            increase="+14%"
+            icon={
+              <AddIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="1,123"
+            subtitle="Total Student"
+            progress="1"
+            increase="+21%"
+            icon={
+              <PersonAddAltIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="1,12,187"
+            subtitle="Our Alumini"
+            progress="0.80"
+            increase="+43%"
+            icon={
+              <TrafficIcon
+                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+              />
+            }
+          />
+        </Box>
+
+        {/* ROW 2 */}
+        <Box
+          gridColumn="span 6"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+        >
+          <Box
+            mt="25px"
+            p="0 30px"
+            display="flex "
+            justifyContent="space-between"
+            alignItems="center"
+          >
+              <Typography
+                variant="h5"
+                fontWeight="600"
+                color={colors.grey[100]}
+              >
+                Average Student Per Semester
+              </Typography>
+              
+            
+          </Box>
+          <Box height="250px" >
+            <PieChart isDashboard={true} />
+          </Box>
+        </Box>
+
+        <Box
+          gridColumn="span 6"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+        >
+          <Box
+            mt="25px"
+            p="0 30px"
+            display="flex "
+            justifyContent="space-between"
+            alignItems="center"
+          >
+              <Typography
+                variant="h5"
+                fontWeight="600"
+                color={colors.grey[100]}
+              >
+                Passing Rate Per Semester
+              </Typography>
+              
+            
+          </Box>
+          <Box height="250px" >
+            <PieChart isDashboard={true} />
+          </Box>
+        </Box>
+        
+
+        {/* ROW 3 */}
+        <Box
+          gridColumn="span 8"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          p="30px"
+        >
+          <Typography variant="h5" fontWeight="600">
+            Average GPA Rate
+          </Typography>
+          <Box height="250px" m="-20px 0 0 0">
+            <LineChart isDashboard={true} />
+          </Box>
+        </Box>
+        
+        <Box
+          gridColumn="span 4"
+          gridRow="span 2"
+          backgroundColor={colors.primary[400]}
+          padding="30px"
+        >
+          <Typography
+            variant="h5"
+            fontWeight="600"
+            sx={{ marginBottom: "15px" }}
+          >
+            Department Based Student's
+          </Typography>
+          <Box height="250px" mt="-20px">
+            <BarChart isDashboard={true} />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default AcademicInsights;
